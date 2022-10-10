2245488c7f5e5628bb06d69b71b05e4e9a482c3d
zy445566 <zyct@vip.qq.com>
2022-10-10 21:56:23 +0800

    feat: init example

---
+++ src/apps/ui/index.ts
@@ -0,0 +1,19 @@
+import {VaasServerType, Decorator} from 'vaas-framework'
+import {promises as fsPromises} from 'fs'
+import * as path from 'path'
+
+export default class UI {
+    @Decorator.VassServer({type:'http',method:'get',routerName:'/'})
+ async index({req,res}:VaasServerType.HttpParams) {
+    return (await fsPromises.readFile(path.join(__dirname,'./public/index.html'))).toString()
+ }
+
+ @Decorator.VassServer({type:'http',method:'get',routerName:'/location'})
+ async location({req,res}:VaasServerType.HttpParams) {
+    res.status=302
+    res.headers = {
+        'location':'https://github.com/virtual-less/vaas-framework'
+    }
+    return `redirect to ${res.headers.location}`
+ }
+}

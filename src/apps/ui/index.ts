5975bf1d4f1b7742aebdfe1d53a10f3eb1ea78d0
zy445566 <zyct@vip.qq.com>
2022-10-11 20:03:14 +0800

    fix: upgrade

--- src/apps/ui/index.ts
+++ src/apps/ui/index.ts
@@ -1,11 +1,18 @@
 import {VaasServerType, Decorator} from 'vaas-framework'
 import {promises as fsPromises} from 'fs'
+import * as fs from 'fs'
 import * as path from 'path'
 
 export default class UI {
     @Decorator.VassServer({type:'http',method:'get',routerName:'/'})
  async index({req,res}:VaasServerType.HttpParams) {
-    return (await fsPromises.readFile(path.join(__dirname,'./public/index.html'))).toString()
+   res.status = 200
+   return (await fsPromises.readFile(path.join(__dirname,'./public/index.html'))).toString()
+ }
+ @Decorator.VassServer({type:'http',method:'get',routerName:'/favicon.ico'})
+ async faviconIco({req,res}:VaasServerType.HttpParams) {
+   res.status = 200
+   return await fsPromises.readFile(path.join(__dirname,'./public/favicon.ico'))
  }
 
  @Decorator.VassServer({type:'http',method:'get',routerName:'/location'})

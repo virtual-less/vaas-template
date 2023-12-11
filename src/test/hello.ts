f6ad1f64d7d38e72778adb8815ba8794933cc012
zy445566 <zyct@vip.qq.com>
2023-12-11 16:44:59 +0800

    feat: 添加测试用例模版

---
+++ src/test/hello.ts
@@ -0,0 +1,7 @@
+import * as assert from 'assert'
+import {describe, it} from 'mocha'
+describe('hello', async function () {
+    it('#hello()', async function () {
+      assert.equal(true, true);
+    });
+});
